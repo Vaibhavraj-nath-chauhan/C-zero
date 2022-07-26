6600b92a04fcb0b4eb02552ee96cd1024658e07d
Vaibhavraj Nath Chauhan <vaibhavrajbanna@gmail.com>
2022-07-26 20:12:11 +0530

    30

---
+++ C++/30 Codes/10. Fill Candies.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	float n,k,m;
+	while(t--){
+	    cin>>n>>k>>m;
+	    cout<<ceil(n/(k*m))<<endl;
+	}
+	
+	return 0;
+}
+
