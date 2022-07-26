4703408d3ddab230c681e8fe331abc65606e6c89
Vaibhavraj Nath Chauhan <vaibhavrajbanna@gmail.com>
2022-07-26 21:08:34 +0530

    30

---
+++ C++/30 Codes/11. Vector at.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+int main(){
+	
+	vector<int> vec{10,20,30,40};
+	cout<<vec.at(3);
+	
+}
---
+++ C++/30 Codes/12. Vecotr back front.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+int main(){
+	
+	vector<int> vec{10,20,30,40};
+	cout<<vec.front()<<endl;
+	cout<<vec.back();
+	
+}
