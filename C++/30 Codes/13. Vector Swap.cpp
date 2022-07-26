57a2cbefbbf1584bff88231f017ff139d656bf0a
Vaibhavraj Nath Chauhan <vaibhavrajbanna@gmail.com>
2022-07-26 21:21:13 +0530

    30

--- C++/30 Codes/13. Vector Swap.cpp
+++ C++/30 Codes/13. Vector Swap.cpp
@@ -4,8 +4,28 @@
 using namespace std;
 int main(){
 	
-	vector<int> vec{10,20,30,40};
-	cout<<vec.front()<<endl;
-	cout<<vec.back();
+	vector<int> v1{10,20,30,40,100};
+	vector<int> v2{50,60,70,80};
+	cout<<"Befor Swap-->\n\tVector 1->\n\t\t";
+	for(auto it=v1.begin();it!=v1.end();it++){
+		cout<<*it<<" ";
+	}
+	cout<<"\n\n\tVector 2->\n\t\t";
+	for(auto it=v2.begin();it!=v2.end();it++){
+		cout<<*it<<" ";
+	}
+	
+	//swap
+	v1.swap(v2);
+	
+	cout<<"\n\nAfter Swap-->\n\tVector 1->\n\t\t";
+	for(auto it=v1.begin();it!=v1.end();it++){
+		cout<<*it<<" ";
+	}
+	cout<<"\n\n\tVector 2->\n\t\t";
+	for(auto it=v2.begin();it!=v2.end();it++){
+		cout<<*it<<" ";
+	}
+	
 	
 }
