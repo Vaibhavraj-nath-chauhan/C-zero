37f86e8d9eec008e588dca95cd138df2fee6d769
Vaibhavraj Nath Chauhan <vaibhavrajbanna@gmail.com>
2022-07-26 23:17:06 +0530

    30;

---
+++ C++/30 Codes/23. Int Out HackerRank.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int age;
+    string first_name;
+    string last_name;
+    int standard;
+    cin>>age>>first_name>>last_name>>standard;
+    cout<<age<<" "<<first_name<<" "<<last_name<<" "<<standard;
+    
+    return 0;
+}
+
