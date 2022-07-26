9a3e2fd4ad7c2def77e53fd1b8426a1369ab67b1
Vaibhavraj Nath Chauhan <vaibhavrajbanna@gmail.com>
2022-07-26 19:25:21 +0530

    30

---
+++ C++/30 Codes/1. Passes for Fair.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	int n,k;
+	while(t--)
+    {
+    	cin>>n>>k;
+    	if(n+1>k){
+    	    cout<<"No"<<endl;
+    	}
+    	else{
+    	    cout<<"YES"<<endl;
+    	}
+	    
+	}
+	return 0;
+}
+
