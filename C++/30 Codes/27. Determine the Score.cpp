c0bcd065500e8840fd8fc5425d70c12b03dbabaf
Vaibhavraj Nath Chauhan <vaibhavrajbanna@gmail.com>
2022-07-26 23:55:52 +0530

    30;

---
+++ C++/30 Codes/27. Determine the Score.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	int x,n;
+	while(t--){
+	    cin>>x>>n;
+	    cout<<(x/10)*n<<endl;
+	}
+	return 0;
+}
+
---
+++ C++/30 Codes/28. Who is taller!.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here'
+	int t;
+	cin>>t;
+	int a,b;
+	while(t--){
+	    cin>>a>>b;
+	    if(a>b) cout<<"A"<<endl;
+	    else cout<<"B"<<endl;
+	}
+	return 0;
+}
+
