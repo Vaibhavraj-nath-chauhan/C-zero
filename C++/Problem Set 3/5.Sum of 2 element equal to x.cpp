2dbca346d47629be51fb4f39d6d268347b4c46bb
Vaibhavraj Nath Chauhan <vaibhavrajbanna@gmail.com>
2022-08-01 07:38:14 +0530

    P3

--- C++/Problem Set 3/5.Sum of 2 element equal to x.cpp
+++ C++/Problem Set 3/5.Sum of 2 element equal to x.cpp
@@ -14,7 +14,32 @@ int main(){
 	for(int i=0;i<n;i++){
 		cout<<arr[i]<<" ";
 	}
-	
-	
+	int x;
+	cin>>x;
+	int count =0;
+	cout<<endl;
+	for(int i =n-1;i>=0;i--){
+		if(x>=arr[i]){
+			x -= arr[i];
+			count +=1;
+		}
+		if(count ==2){
+			break;
+		}
+		if(x==0){
+			break;
+		}
+	}
+	if(count ==2){
+		if(x==0){
+			cout<<"YES";
+		}
+		else{
+			cout<<"No";
+		}
+	}
+	else{
+		cout<<"No";
+	}
 	
 }
